c8e66cd7ed113e4187b04ac37eddf82ffc81caa7
Francisco Rivas <fran.workbench@gmail.com>
2025-12-10 18:58:47 -0400

    vue changes

--- packages/frontend/@n8n/design-system/src/components/DateRangePicker/DateRangePicker.vue
+++ packages/frontend/@n8n/design-system/src/components/DateRangePicker/DateRangePicker.vue
@@ -17,7 +17,7 @@ import {
 	DateRangePickerTrigger,
 	useForwardPropsEmits,
 } from 'reka-ui';
-import { useSlots } from 'vue';
+
 
 import Button from '../N8nButton/Button.vue';
 import IconButton from '../N8nIconButton';
@@ -33,7 +33,7 @@ const props = withDefaults(defineProps<N8nDateRangePickerProps>(), {
 
 const emit = defineEmits<N8nDateRangePickerRootEmits>();
 const forwarded = useForwardPropsEmits(props, emit);
-const slots = useSlots();
+
 </script>
 
 <template>
@@ -46,7 +46,7 @@ const slots = useSlots();
 
 		<DateRangePickerContent align="start" :side-offset="5" :class="$style.PopoverContent">
 			<DateRangePickerCalendar v-slot="{ weekDays, grid }" :class="$style.Calendar">
-				<template v-if="!!slots.presets">
+				<template v-if="!!$slots.presets">
 					<div :class="$style.Presets">
 						<slot name="presets" />
 					</div>
